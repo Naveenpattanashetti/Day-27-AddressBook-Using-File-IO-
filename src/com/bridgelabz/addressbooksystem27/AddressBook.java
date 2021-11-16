87757aaf1fc5401b62c7002e267f36d4b8323cc5
Naveen Pattanashetti <pattanashettinaveen.com>
2021-11-16 20:50:29 +0530

    Program to Create A contacts in Address book

---
+++ src/com/bridgelabz/addressbooksystem27/AddressBook.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.addressbooksystem27;
+
+import java.util.List;
+
+public class AddressBook {
+	private List<Contact> contact;
+
+	public List<Contact> getContacts() {
+		return contact;
+	}
+
+	public void setContacts(List<Contact> contacts) {
+		this.contact = contacts;
+	}
+
+}

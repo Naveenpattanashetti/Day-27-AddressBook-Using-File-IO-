9a5f7b3a5f4857a5aa14f6a528e5adb7f00e0bad
Naveen Pattanashetti <pattanashettinaveen.com>
2021-11-16 20:42:54 +0530

    wel come to user

---
+++ src/com/bridgelabz/addressbooksystem27/AddressBookMainClass.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.addressbooksystem27;
+
+public class AddressBookMainClass {
+
+	public static void main(String[] args) {
+		System.out.println("wel come to address system");
+
+	}
+
+}
